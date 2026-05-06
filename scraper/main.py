b45a409a6ff350ecc624c212c61254cc28317cb0
Theodora Giannouli <doragiannul@gmail.com>
2026-05-06 22:24:55 +0300

    Publish jobs to RabbitMQ

--- scraper/main.py
+++ scraper/main.py
@@ -1,6 +1,28 @@
-from jsonschema import validate, ValidationError, FormatChecker
 import json
+import os
 
+import pika
+from dotenv import load_dotenv
+from jsonschema import FormatChecker, ValidationError, validate
+
+load_dotenv()
+
+RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
+RABBITMQ_USER = os.getenv("RABBITMQ_USER", "guest")
+RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "guest")
+QUEUE_NAME = os.getenv("QUEUE_NAME", "jobs_raw")
+
+# connect to RMQ
+credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=RABBITMQ_HOST, credentials=credentials)
+)
+channel = connection.channel()
+
+# create queue
+channel.queue_declare(queue=QUEUE_NAME, durable=True)
+
+# schema to use for validating ingesting jobs
 valid_schema = {
     "type": "object",
     "properties": {
@@ -12,21 +34,30 @@ valid_schema = {
         "salary_min": {"type": ["number", "null"], "minimum": 0},
         "salary_max": {"type": ["number", "null"], "minimum": 0},
         "url": {"type": "string", "format": "uri"},
-        "posted_at": {"type": "string", "format": "date-time"}
-},
-    "required": ["external_id", "title", "company", "location", "description", "url"]
+        "posted_at": {"type": "string", "format": "date-time"},
+    },
+    "required": ["external_id", "title", "company", "location", "description", "url"],
 }
 
-with open('data/mock_jobs.json', 'r') as file:
+# read jobs from mock data in json file
+with open("data/mock_jobs.json", "r") as file:
     jobs = json.load(file)
-    #print(json.dumps(jobs, indent=4))
+    # print(json.dumps(jobs, indent=4))
 
 valid_jobs = 0
 invalid_jobs = 0
 for job in jobs:
-    # validate job item
+    # validate & publish job item
     try:
         validate(instance=job, schema=valid_schema, format_checker=FormatChecker())
+
+        channel.basic_publish(
+            exchange="",
+            routing_key=QUEUE_NAME,
+            body=json.dumps(job),
+            properties=pika.BasicProperties(),
+        )
+        print(f"Published: {job['external_id']}")
         valid_jobs += 1
     except ValidationError as error:
         job_id = job.get("external_id", "unknown")
@@ -34,6 +65,7 @@ for job in jobs:
         print(f"Reason: {error.message}")
         invalid_jobs += 1
         continue
-    
-print(f"Found {valid_jobs} valid jobs and {invalid_jobs} invalid jobs")
 
+connection.close()
+print("Done. Connection to RMQ closed.")
+print(f"Found {valid_jobs} valid jobs and {invalid_jobs} invalid jobs")
